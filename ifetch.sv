bb2921d47a7359c7624fb757805a99437944bfa1
marisolcastellanos <116708611+marisolcastellanos@users.noreply.github.com>
2022-11-07 15:04:32 -0800

    Update ifetch.sv

--- ifetch.sv
+++ ifetch.sv
@@ -1,26 +1,27 @@
 //IF stage 
 module ifetch (
+   input logic          clk,reset,
    input  logic         PCSrcE,
-   input  logic  [31:0] PCPlus4F,PCTargetE,
+   input  logic  [31:0] PCTargetE,
    output logic  [31:0] InstrD,
    output logic  [31:0] PCD,PCPlus4D   
    );
 
 //signals
    logic [31:0] PCNext;
-   logic [31:0] InstrF;
+   logic [31:0] InstrF, PCPlus4F;
 
    //instantiations
-   mux2 #(32)  pcmux(PCPlus4F, PCTargetE, PCSrcE, PCNext);
+   mux2 #(32)  pcmux(PCPlus4F, PCTargetE, PCSrcE, PCNext); //PCNext is equivalent to PCF
 
    // next PC logic
   flopr #(32) pcreg(clk, reset, PCNext, PCF); 
   adder       pcadd4(PCF, 32'd4, PCPlus4F);
    // Fetch next Instruction 
-  imem imem(PCF, InstrF);
-  // Ifetch pipeline register
+  imem imem1(PCF, InstrF);
   
-   if_id  if_id1  (InstrF, PCF,PCPlus4F,InstrD,PCD,PCPlus4D);
+   // Ifetch pipeline register
+   if_id  if_id1  (clk,reset,InstrF, PCF,PCPlus4F,InstrD,PCD,PCPlus4D);
 
   
    initial begin
