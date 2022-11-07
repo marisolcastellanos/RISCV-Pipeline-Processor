e13936d9f5425e5482e94e1f308b82eeaa494a93
marisolcastellanos <116708611+marisolcastellanos@users.noreply.github.com>
2022-11-07 15:11:26 -0800

    Update if_id.sv

--- if_id.sv
+++ if_id.sv
@@ -1,17 +1,18 @@
 module if_id (
-  
+   input logic          clk, reset,
    input lgic [31:0]    InstrF,        // Input of IF/ID Instruction Register
-                          PCF,         // Input of IF/ID NPC Register
-						 PCPlus4F,   // Input of IF/ID for Jump target address
+                        PCF,         // Input of IF/ID NPC Register
+			PCPlus4F,   // Input of IF/ID for Jump target address
    output logic [31:0] InstrD,       // Output of IF/ID Instruction Register
                        PCD,        // Output of IF/ID NPC Register
 					   PCPlus4D     // Output of IF/ID for Jump target address
    );
+  always_ff @(posedge clk, posedge reset)
    initial begin
       instrout <= 0;
       npcout   <= 0;
    end
-  lways_ff @(posedge clk, posedge reset)
+  always_ff @(posedge clk, posedge reset)
     if (reset) begin
 		PCD <= 0;
 		InstrD <=0;
