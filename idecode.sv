7e5a7c1ba2bc18cfb835adacc2d9404e236e7008
marisolcastellanos <116708611+marisolcastellanos@users.noreply.github.com>
2022-11-14 15:12:27 -0800

    Update idecode.sv

--- idecode.sv
+++ idecode.sv
@@ -1,4 +1,9 @@
 module idecode ( 
-                input logic clk,reset,
-                input logic [31:0] InstrD, 
-                input logic [31:0] PCD, PCPlus4D
+                input  logic        clk,reset,
+                input  logic [31:0] InstrD, 
+                input  logic [31:0] PCD, PCPlus4D,
+                input  logic        RegWriteW,
+                input  logic        ResultW,
+                input  logic        RdW,
+                );
+endmodule
