e118fd8fc81c5bb15997f3492d343b5ef445eaa2
marisolcastellanos <116708611+marisolcastellanos@users.noreply.github.com>
2022-11-14 15:09:04 -0800

    Update id_ex.sv

--- id_ex.sv
+++ id_ex.sv
@@ -1,3 +1,28 @@
-module if_id( 
-              input logic clk,reset, 
-              input 
+module id_ex(
+        input logic         clk,   reset, 
+        input logic  [4:0]  RdD, RD1D, RD2D,
+        input logic  [31:0] PCD,  
+                            ImmExtD, 
+                            PCPlus4D,
+        output logic [4:0]  RdE, RD1E, RD2E,
+        output logic [31:0] PCE, 
+                            ImmExtE,
+                            PCPlus4E 
+                                                        
+    );
+
+    always_ff @(posedge clk, posedge reset) 
+    if (reset) begin 
+    
+    
+    end 
+    else begin
+    PCE <= PCD; 
+    RdE <= RdD;
+    RD1E <= RD1D;
+    RD2E <= RD2D; 
+    ImmExtE <= ImmExtD; 
+    PCPlus4E <= PCPlus4D; 
+    end
+
+endmodule
